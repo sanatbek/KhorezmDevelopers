51139d6d60bef00003b6b5c7a425158ee4c0bd83
sanatbek <mr.sanatbek_94@mail.ru>
2015-03-13 15:06:59 +0500

    readme is added

--- app/controllers/Developers.scala
+++ app/controllers/Developers.scala
@@ -1,5 +1,8 @@
 package controllers
-
-object Developers {
-
+import models.Presentation
+import play.api.mvc._
+object Developers extends Controller{
+  def prList = Action {
+    Ok(views.html.presentations(Presentation.findPresentations))
+  }
 }
